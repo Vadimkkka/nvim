20ab73a5da984fe73063f81ed98e460d419d70f3
Vadimkkka <vadom99@mail.ru>
2023-09-28 22:03:33 +0300

    feat(plugins/treesitter): Add clang, cpp 👅

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "html", "css", "javascript", "typescript", "vue", "scss", "comment", "lua", "rust" },
+  ensure_installed = { "html", "css", "javascript", "typescript", "vue", "scss", "comment", "lua", "rust", "c", "cpp" },
   highlight = { enable = true },
   rainbow = { enable = true },
 }
