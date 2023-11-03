fee449a896038fd4de5dad1fd99c566f19670349
Vadimkkka <vadom99@mail.ru>
2023-11-03 09:48:40 +0300

    feat(core): Mappings. Close current buffer

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,6 +1,9 @@
 vim.keymap.set('n', '<C-n>', ':NvimTreeFocus<CR>')
 vim.keymap.set('n', '<C-т>', ':NvimTreeFocus<CR>')
 
+vim.keymap.set('n', '<tab>q', ':BufferLineCycleNext<CR> <BAR> :BufferLineCloseLeft<CR>')
+vim.keymap.set('n', '<tab>й', ':BufferLineCycleNext<CR> <BAR> :BufferLineCloseLeft<CR>')
+
 vim.keymap.set('n', '<tab>w', ':BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<tab>ц', ':BufferLineCyclePrev<CR>')
 
