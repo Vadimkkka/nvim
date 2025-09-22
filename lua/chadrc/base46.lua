f4abf3c2ef8a93fd429ae88e008ffca6854d6e58
Vadimkkka <viltovskiy.v@promail366.com>
2025-09-22 14:14:22 +0300

    fix(chadrc): flash plugin

--- lua/chadrc/base46.lua
+++ lua/chadrc/base46.lua
@@ -3,7 +3,7 @@ local M = {
   -- jabuti, kanagawa, onedark, palenight, yoru, tokyonight, tokyodark, oceanic-next, nightfox, material-deep-ocean
   theme = "catppuccin",
   transparency = true,
-  integrations = { "rainbowdelimiters", "todo", "flash" },
+  integrations = { "rainbowdelimiters", "todo" },
 }
 
 M.hl_add = {
@@ -17,9 +17,9 @@ M.hl_add = {
   -- TerminalMode
   -- TerminalNormalMode
 
-  -- FlashMatch = { fg = "black", bg = "blue" },
-  -- FlashCurrent = { fg = "black", bg = "green" },
-  -- FlashLabel = { fg = "white", bold = true },
+  FlashMatch = { fg = "black", bg = "blue" },
+  FlashCurrent = { fg = "black", bg = "green" },
+  FlashLabel = { fg = "white", bold = true },
 }
 
 M.hl_override = {
